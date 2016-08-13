a5f9d02224957042ee28b1b96b374ee78023b8f4
Bryan Brophy <brybrophy@gmail.com>
2016-08-12 22:32:56 -0700

    Layout public folder

---
+++ public/js/app.config.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('workflowApp')
+    .config(config);
+
+    // function config($routeProvider) {
+    //   $routeProvider
+    //     .when('/', {
+    //       templateUrl: 'views/home.html',
+    //       controller: 'PostsCtrl',
+    //       controllerAs: 'posts'
+    //     });
+    // };
+})();
