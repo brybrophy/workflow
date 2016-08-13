1172f97ea34d988ed8a10e12457ce91058d6cc5c
Bryan Brophy <brybrophy@gmail.com>
2016-08-12 18:42:17 -0700

    Write server.js file

---
+++ server.js
@@ -0,0 +1,72 @@
+'use strict';
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({ silent: true });
+}
+
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8000;
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+
+const app = express();
+
+const users = require('./routes/users');
+const topics = require('./routes/jobs');
+const posts = require('./routes/contacts');
+const posts = require('./routes/contacts_jobs');
+const token = require('./routes/token');
+
+app.disable('x-powered-by');
+
+switch (app.get('env')) {
+  case 'development':
+    app.use(morgan('dev'));
+    break;
+
+  case 'production':
+    app.use(morgan('short'));
+    break;
+
+  default:
+}
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(users);
+app.use(jobs);
+app.use(contacts);
+app.use(contacts_jobs);
+app.use(token);
+
+app.use((_req, res) => {
+  res.sendStatus(404);
+});
+
+// eslint-disable-next-line max-params
+app.use((err, _req, res, _next) => {
+  if (err.status || (err.output && err.output.statusCode)) {
+    return res
+      .status(err.status || err.output.statusCode)
+      .send(err);
+  }
+
+  // eslint-disable-next-line no-console
+  console.error(err.stack);
+  res.sendStatus(500);
+});
+
+app.listen(port, () => {
+  if (process.env.NODE_ENV !== 'test') {
+    // eslint-disable-next-line no-console
+    console.log('Listening on port', port);
+  }
+});
+
+module.exports = app;
