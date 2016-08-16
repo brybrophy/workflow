81ce5fd8306887f852f7cb1f990e53c5406d63fd
Jake Bell <jakebrbell@gmail.com>
2016-08-15 23:01:09 -0700

    Close #11 Write migrations tests

---
+++ tests/migrations.test.js
@@ -0,0 +1,531 @@
+'use strict';
+
+process.env.NODE_ENV = 'test';
+
+const assert = require('chai').assert;
+const { suite, test } = require('mocha');
+const knex = require('../knex');
+
+suite('Migrations', () => {
+  before((done) => {
+    knex.migrate.latest()
+      .then(() => {
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  test('Users columns', (done) => {
+    knex('users').columnInfo()
+      .then((actual) => {
+        const expected = {
+          id: {
+            type: 'integer',
+            maxLength: null,
+            nullable: false,
+            defaultValue: 'nextval(\'users_id_seq\'::regclass)'
+          },
+
+          username: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: '\'\'::character varying'
+          },
+
+          email: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: '\'\'::character varying'
+          },
+
+          hashed_password: {
+            type: 'character',
+            maxLength: 60,
+            nullable: false,
+            defaultValue: null
+          },
+
+          created_at: {
+            type: 'timestamp with time zone',
+            maxLength: null,
+            nullable: false,
+            defaultValue: 'now()'
+          },
+
+          updated_at: {
+            type: 'timestamp with time zone',
+            maxLength: null,
+            nullable: false,
+            defaultValue: 'now()'
+          }
+        };
+
+        for (const column in expected) {
+          assert.deepEqual(
+            actual[column],
+            expected[column],
+            `Column ${column} is not the same`
+          );
+        }
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  test('Jobs columns', (done) => {
+    knex('jobs').columnInfo()
+      .then((actual) => {
+        const expected = {
+          id: {
+            type: 'integer',
+            maxLength: null,
+            nullable: false,
+            defaultValue: 'nextval(\'jobs_id_seq\'::regclass)'
+          },
+
+          title: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: '\'\'::character varying'
+          },
+
+          job_post_url: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: '\'\'::character varying'
+          },
+
+          company_name: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: '\'\'::character varying'
+          },
+
+          company_address_1: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: '\'\'::character varying'
+          },
+
+          company_address_2: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: '\'\'::character varying'
+          },
+
+          company_city: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: '\'\'::character varying'
+          },
+
+          company_state: {
+            type: 'character',
+            maxLength: 2,
+            nullable: false,
+            defaultValue: '\'\'::bpchar'
+          },
+
+          company_zip: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: '\'\'::character varying'
+          },
+
+          company_website: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: '\'\'::character varying'
+          },
+
+          interview_status: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: '\'\'::character varying'
+          },
+
+          interview_informational: {
+            type: 'timestamp with time zone',
+            maxLength: null,
+            nullable: true,
+            defaultValue: null
+          },
+
+          interview_applied: {
+            type: 'timestamp with time zone',
+            maxLength: null,
+            nullable: true,
+            defaultValue: null
+          },
+
+          interview_phone: {
+            type: 'timestamp with time zone',
+            maxLength: null,
+            nullable: true,
+            defaultValue: null
+          },
+
+          interview_technical: {
+            type: 'timestamp with time zone',
+            maxLength: null,
+            nullable: true,
+            defaultValue: null
+          },
+
+          interview_onsite: {
+            type: 'timestamp with time zone',
+            maxLength: null,
+            nullable: true,
+            defaultValue: null
+          },
+
+          interview_take_home: {
+            type: 'timestamp with time zone',
+            maxLength: null,
+            nullable: true,
+            defaultValue: null
+          },
+
+          interview_offer: {
+            type: 'timestamp with time zone',
+            maxLength: null,
+            nullable: true,
+            defaultValue: null
+          },
+
+          notes: {
+            type: 'text',
+            maxLength: null,
+            nullable: false,
+            defaultValue: '\'\'::text'
+          },
+
+          user_id: {
+            type: 'integer',
+            maxLength: null,
+            nullable: false,
+            defaultValue: null
+          },
+
+          interview_rejected: {
+            type: 'timestamp with time zone',
+            maxLength: null,
+            nullable: true,
+            defaultValue: null
+          },
+
+          created_at: {
+            type: 'timestamp with time zone',
+            maxLength: null,
+            nullable: false,
+            defaultValue: 'now()'
+          },
+
+          updated_at: {
+            type: 'timestamp with time zone',
+            maxLength: null,
+            nullable: false,
+            defaultValue: 'now()'
+          }
+        };
+
+        for (const column in expected) {
+          assert.deepEqual(
+            actual[column],
+            expected[column],
+            `Column ${column} is not the same`
+          );
+        }
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  test('Jobs constraints', (done) => {
+    const query = `
+      SELECT
+        tc.table_name, kcu.column_name,
+        ccu.table_name AS foreign_table_name,
+        ccu.column_name AS foreign_column_name
+      FROM
+        information_schema.table_constraints AS tc
+        JOIN information_schema.key_column_usage AS kcu
+          ON tc.constraint_name = kcu.constraint_name
+        JOIN information_schema.constraint_column_usage AS ccu
+          ON ccu.constraint_name = tc.constraint_name
+      WHERE constraint_type = 'FOREIGN KEY' AND tc.table_name='jobs';
+    `;
+
+    knex.raw(query)
+      .then((result) => {
+        const actual = result.rows;
+
+        /* eslint-disable-next-line camelcase */
+        const expected = [{
+          table_name: 'jobs',
+          column_name: 'user_id',
+          foreign_table_name: 'users',
+          foreign_column_name: 'id'
+        }];
+
+        for (const column in expected) {
+          assert.deepEqual(
+            actual[column],
+            expected[column],
+            `Column ${column} is not the same`
+          );
+        }
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  test('Contacts columns', (done) => {
+    knex('contacts').columnInfo()
+      .then((actual) => {
+        const expected = {
+          id: {
+            type: 'integer',
+            maxLength: null,
+            nullable: false,
+            defaultValue: 'nextval(\'contacts_id_seq\'::regclass)'
+          },
+
+          first_name: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: '\'\'::character varying'
+          },
+
+          last_name: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: '\'\'::character varying'
+          },
+
+          email: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: '\'\'::character varying'
+          },
+
+          phone: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: '\'\'::character varying'
+          },
+
+          linked_in_url: {
+            type: 'character varying',
+            maxLength: 255,
+            nullable: false,
+            defaultValue: '\'\'::character varying'
+          },
+
+          user_id: {
+            type: 'integer',
+            maxLength: null,
+            nullable: false,
+            defaultValue: null
+          },
+
+          created_at: {
+            type: 'timestamp with time zone',
+            maxLength: null,
+            nullable: false,
+            defaultValue: 'now()'
+          },
+
+          updated_at: {
+            type: 'timestamp with time zone',
+            maxLength: null,
+            nullable: false,
+            defaultValue: 'now()'
+          }
+        };
+
+        for (const column in expected) {
+          assert.deepEqual(
+            actual[column],
+            expected[column],
+            `Column ${column} is not the same`
+          );
+        }
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  test('Contacts constraints', (done) => {
+    const query = `
+      SELECT
+        tc.table_name, kcu.column_name,
+        ccu.table_name AS foreign_table_name,
+        ccu.column_name AS foreign_column_name
+      FROM
+        information_schema.table_constraints AS tc
+        JOIN information_schema.key_column_usage AS kcu
+          ON tc.constraint_name = kcu.constraint_name
+        JOIN information_schema.constraint_column_usage AS ccu
+          ON ccu.constraint_name = tc.constraint_name
+      WHERE constraint_type = 'FOREIGN KEY' AND tc.table_name='contacts';
+    `;
+
+    knex.raw(query)
+      .then((result) => {
+        const actual = result.rows;
+
+        /* eslint-disable-next-line camelcase */
+        const expected = [{
+          table_name: 'contacts',
+          column_name: 'user_id',
+          foreign_table_name: 'users',
+          foreign_column_name: 'id'
+        }];
+
+        for (const column in expected) {
+          assert.deepEqual(
+            actual[column],
+            expected[column],
+            `Column ${column} is not the same`
+          );
+        }
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  test('Contacts_jobs columns', (done) => {
+    knex('contacts_jobs').columnInfo()
+      .then((actual) => {
+        const expected = {
+          id: {
+            type: 'integer',
+            maxLength: null,
+            nullable: false,
+            defaultValue: 'nextval(\'contacts_jobs_id_seq\'::regclass)'
+          },
+
+          contact_id: {
+            type: 'integer',
+            maxLength: null,
+            nullable: false,
+            defaultValue: null
+          },
+
+          job_id: {
+            type: 'integer',
+            maxLength: null,
+            nullable: false,
+            defaultValue: null
+          },
+
+          created_at: {
+            type: 'timestamp with time zone',
+            maxLength: null,
+            nullable: false,
+            defaultValue: 'now()'
+          },
+
+          updated_at: {
+            type: 'timestamp with time zone',
+            maxLength: null,
+            nullable: false,
+            defaultValue: 'now()'
+          }
+        };
+
+        for (const column in expected) {
+          assert.deepEqual(
+            actual[column],
+            expected[column],
+            `Column ${column} is not the same`
+          );
+        }
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  test('Contacts_jobs constraints', (done) => {
+    const query = `
+      SELECT
+        tc.table_name, kcu.column_name,
+        ccu.table_name AS foreign_table_name,
+        ccu.column_name AS foreign_column_name
+      FROM
+        information_schema.table_constraints AS tc
+        JOIN information_schema.key_column_usage AS kcu
+          ON tc.constraint_name = kcu.constraint_name
+        JOIN information_schema.constraint_column_usage AS ccu
+          ON ccu.constraint_name = tc.constraint_name
+      WHERE constraint_type = 'FOREIGN KEY' AND tc.table_name='contacts_jobs';
+    `;
+
+    knex.raw(query)
+      .then((result) => {
+        const actual = result.rows;
+
+        /* eslint-disable-next-line camelcase */
+        const expected = [{
+          table_name: 'contacts_jobs',
+          column_name: 'contact_id',
+          foreign_table_name: 'contacts',
+          foreign_column_name: 'id'
+        }, {
+          table_name: 'contacts_jobs',
+          column_name: 'job_id',
+          foreign_table_name: 'jobs',
+          foreign_column_name: 'id'
+        }];
+
+        for (const column in expected) {
+          assert.deepEqual(
+            actual[column],
+            expected[column],
+            `Column ${column} is not the same`
+          );
+        }
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+});
