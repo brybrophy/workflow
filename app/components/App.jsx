69e6c3baa848c61be2c012f4e50183d795a9eff9
Bryan Brophy <brybrophy@gmail.com>
2016-08-18 13:03:09 -0700

    Add index.html back in

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,13 +1,18 @@
 import RaisedButton from 'material-ui/RaisedButton';
+import { Parallax, Background } from 'react-parallax';
 import React from 'react';
 
 const App = React.createClass({
   render() {
     return <div>
-      <RaisedButton label="Hello Jake!" />
-
-      <img src="cloud_1.svg" />
-      <img src="cloud_2.svg" />
+      <Parallax strength={300}>
+        <Background>
+          <img src="http://www.fillmurray.com/400/300"/>
+          <div style={{width: 800, height: 300, backgroundColor: '#450093'}}></div>
+          <img src="http://www.fillmurray.com/500/300"/>
+        </Background>
+        <h1>A collection of textile samples lay spread out on the table ...</h1>
+      </Parallax>
     </div>
   }
 });
