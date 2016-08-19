adbba218aa01371ca1b862004306957e27a1782e
Jake Bell <jakebrbell@gmail.com>
2016-08-18 17:05:20 -0700

    Create skeleton for main app bar

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,18 +1,58 @@
-import RaisedButton from 'material-ui/RaisedButton';
-import { Parallax, Background } from 'react-parallax';
+import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
+import NavLogo from 'components/NavLogo';
 import React from 'react';
 
 const App = React.createClass({
   render() {
+
+    const styleAppBar = {
+      height: '125px'
+    };
+
+    const styleFlatButton = {
+      height: '125px',
+      lineHeight: '125px',
+      minWidth: '60px'
+    };
+
+    const styleFlatButtonLabel = {
+      fontSize: '.7rem',
+      padding: '0'
+    };
+
+    const styleIconLeft = {
+      marginLeft: '35px'
+    };
+
     return <div>
-      <Parallax strength={300}>
-        <Background>
-          <img src="http://www.fillmurray.com/400/300"/>
-          <div style={{width: 800, height: 300, backgroundColor: '#450093'}}></div>
-          <img src="http://www.fillmurray.com/500/300"/>
-        </Background>
-        <h1>A collection of textile samples lay spread out on the table ...</h1>
-      </Parallax>
+      <AppBar
+        style={styleAppBar}
+        title="workflow | Job Search Management"
+        iconElementLeft={<NavLogo/>}
+        iconStyleLeft={styleIconLeft}
+      >
+        <FlatButton
+          label="Dashboard"
+          labelStyle={styleFlatButtonLabel}
+          style={styleFlatButton}
+        />
+        <FlatButton
+          label="Jobs"
+          labelStyle={styleFlatButtonLabel}
+          style={styleFlatButton}
+        />
+        <FlatButton
+          label="Contacts"
+          labelStyle={styleFlatButtonLabel}
+          style={styleFlatButton}
+        />
+        <FlatButton
+          label="Logout"
+          labelStyle={styleFlatButtonLabel}
+          style={styleFlatButton}
+        />
+      </AppBar>
     </div>
   }
 });
--- app/index.jsx
+++ app/index.jsx
@@ -2,12 +2,24 @@ import App from 'components/App';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 injectTapEventPlugin();
 
+const muiTheme = getMuiTheme({
+  flatButton: {
+    buttonFilterColor: '#7ECBEA',
+    textColor: 'white'
+  },
+
+  palette: {
+    primary1Color: '#47B4E0'
+  }
+});
+
 ReactDOM.render(
-  <MuiThemeProvider>
+  <MuiThemeProvider muiTheme={muiTheme}>
     <App />
   </MuiThemeProvider>,
   document.getElementById('app')
