84fb1ea64bffaca2d53d82efb995cda58968333f
Bryan Brophy <brybrophy@gmail.com>
2016-08-20 09:23:56 -0700

    Refactor MainNavBar and Footer into components

---
+++ app/components/Footer.jsx
@@ -0,0 +1,72 @@
+import FlatButton from 'material-ui/FlatButton';
+import FooterLogo from 'components/FooterLogo';
+import GithubLogo from 'components/GithubLogo';
+import React from 'react';
+import TwitterLogo from 'components/TwitterLogo';
+
+const Footer = React.createClass({
+  render() {
+    const styleFooter = {
+      backgroundColor: 'black',
+      display: 'flex',
+      height: '120px',
+      width: '100vw'
+    };
+
+    const styleFlatButtonBottom = {
+      height: '120px',
+      lineHeight: '120px',
+      padding: '20px 0 0'
+    };
+
+    const styleFlatButtonLabel = {
+      fontFamily: 'MontserratHairline',
+      fontSize: '1.2rem',
+      padding: '0'
+    };
+
+    const styleFooterLogo = {
+      marginLeft: '50px',
+      marginTop: '25px'
+    };
+
+    const stylePullRight = {
+      position: 'absolute',
+      right: '50px'
+    };
+
+    const styleTextBottom = {
+      color: 'white',
+      cursor: 'pointer',
+      fontFamily: 'MontserratHairline',
+      fontSize: '1.1rem',
+      letterSpacing: '0.12rem',
+      marginLeft: '10px',
+      marginTop: '70px'
+    };
+
+    return <footer style={styleFooter}>
+
+      <div style={styleFooterLogo}>
+        <FooterLogo />
+      </div>
+
+      <p className="hairline-font space-letters" style={styleTextBottom}>
+        workflow © 2016-2017
+      </p>
+
+      <div style={stylePullRight}>
+        <FlatButton
+          label={<GithubLogo />}
+          style={styleFlatButtonBottom}
+        />
+        <FlatButton
+          label={<TwitterLogo />}
+          style={styleFlatButtonBottom}
+        />
+      </div>
+    </footer>
+  }
+});
+
+export default Footer;
---
+++ app/components/MainNav.jsx
@@ -0,0 +1,97 @@
+import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
+import NavLogo from 'components/NavLogo';
+import React from 'react';
+
+const MainNav = React.createClass({
+  render() {
+
+    const styleAppBar = {
+      height: '125px',
+      paddingRight: '70px'
+    };
+
+    const styleFlatButton = {
+      height: '125px',
+      lineHeight: '125px',
+      minWidth: '60px'
+    };
+
+    const styleFlatButtonLabel = {
+      fontFamily: 'MontserratHairline',
+      fontSize: '1.2rem',
+      letterSpacing: '0.12rem',
+      padding: '0'
+    };
+
+    const styleIconLeft = {
+      height: '125px',
+      lineHeight: '125px',
+      marginLeft: '35px',
+      marginTop: '20px'
+    };
+
+    const styleNavbarLine = {
+      color: 'white',
+      fontFamily: 'MontserratHairline',
+      fontSize: '2rem',
+      marginLeft: '18px',
+      marginRight: '15px',
+      paddingTop: '30px'
+    };
+
+    const styleSubTitle = {
+      fontSize: '1.7rem',
+      marginLeft: '10px',
+      position: 'relative',
+      top: '-2px'
+    };
+
+    const styleTitle = {
+      cursor: 'pointer',
+      fontFamily: 'MontserratHairline',
+      fontSize: '1.2rem',
+      height: '125px',
+      letterSpacing: '0.12rem',
+      lineHeight: '71px',
+      marginTop: '20px'
+    };
+
+
+    return <AppBar
+      iconElementLeft={<NavLogo />}
+      iconStyleLeft={styleIconLeft}
+      style={styleAppBar}
+      title={<h1>workflow |
+        <span style={styleSubTitle}>Job Search Management</span>
+      </h1>}
+        titleStyle={styleTitle}
+      >
+      <FlatButton
+        label="Dashboard"
+        labelStyle={styleFlatButtonLabel}
+        style={styleFlatButton}
+      />
+      <FlatButton
+        label="Jobs"
+        labelStyle={styleFlatButtonLabel}
+        style={styleFlatButton}
+      />
+      <FlatButton
+        label="Contacts"
+        labelStyle={styleFlatButtonLabel}
+        style={styleFlatButton}
+      />
+
+      <h1 style={styleNavbarLine}> | </h1>
+
+      <FlatButton
+        label="Logout"
+        labelStyle={styleFlatButtonLabel}
+        style={styleFlatButton}
+      />
+    </AppBar>
+  }
+});
+
+export default MainNav;
