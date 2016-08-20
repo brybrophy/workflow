69cba1e5de6acd6f3b3100abf1622058965cdbc4
Jake Bell <jakebrbell@gmail.com>
2016-08-20 15:58:05 -0700

    Creat JobInfo component

---
+++ app/components/JobInfo.jsx
@@ -0,0 +1,21 @@
+import JobForm from 'components/JobForm';
+import JobMap from 'components/JobMap';
+import React from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
+
+const JobInfo = React.createClass({
+  render() {
+    return <Grid style={{maxWidth: '960px'}}>
+      <Row>
+        <Col xs={12} sm={7}>
+          <JobForm />
+        </Col>
+        <Col xs={12} sm={4} smOffset={1}>
+          <JobMap />
+        </Col>
+      </Row>
+    </Grid>;
+  }
+});
+
+export default JobInfo;
