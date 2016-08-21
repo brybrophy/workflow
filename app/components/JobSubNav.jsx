c9787088e6f42a9a167ed5fd6332afbb68a6e781
Jake Bell <jakebrbell@gmail.com>
2016-08-20 17:39:34 -0700

    Close #15 Create job view sub-navigation

---
+++ app/components/JobSubNav.jsx
@@ -0,0 +1,24 @@
+import { Tab, Tabs } from 'material-ui/Tabs';
+import React from 'react';
+
+const JobSubNav = React.createClass({
+  render() {
+    const styleTab = {
+      color: 'black',
+      fontFamily: 'MontserratLight'
+    }
+
+    return <Tabs
+      inkBarStyle={{ backgroundColor: '#47B4E0' }}
+      style={{ margin: '0 auto', width: '50%' }}
+      tabItemContainerStyle={{ backgroundColor: 'none' }}
+    >
+      <Tab label="Job" style={styleTab} />
+      <Tab label="Contacts" style={styleTab} />
+      <Tab label="Progress" style={styleTab} />
+      <Tab label="Notes" style={styleTab} />
+    </Tabs>;
+  }
+});
+
+export default JobSubNav;
