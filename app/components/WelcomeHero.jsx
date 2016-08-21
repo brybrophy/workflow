6371cc0138dacceee309a6ee146e7b80d8cc98cd
Bryan Brophy <brybrophy@gmail.com>
2016-08-21 09:07:18 -0700

    Finish Hero (Not Fully Responsive)

--- app/components/WelcomeHero.jsx
+++ app/components/WelcomeHero.jsx
@@ -1,4 +1,5 @@
 import PipesHero from 'components/PipesHero';
+import RaisedButton from 'material-ui/RaisedButton';
 import React from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 
@@ -6,9 +7,9 @@ const WelcomeHero = React.createClass({
   render() {
     const styleCloud3 = {
       display: 'inline-block',
-      left: '90px',
-      position: 'relative',
-      top: '150px',
+      position: 'absolute',
+      right: '450px',
+      top: '70px',
       width: '200px'
     };
 
@@ -19,11 +20,16 @@ const WelcomeHero = React.createClass({
 
     const styleHeroHeader = {
       color: 'white',
-      display: 'inline-block',
       fontFamily: 'MontserratLight',
       position: 'relative',
       textAlign: 'center',
-      top: '225px'
+      top: '170px'
+    }
+
+    const styleHeroText= {
+      fontFamily: 'MontserratHairline',
+      fontSize: '2rem',
+      letterSpacing: '0.12rem'
     }
 
     const stylePipes = {
@@ -31,17 +37,52 @@ const WelcomeHero = React.createClass({
       left: '48px'
     }
 
+    const styleRaisedButton = {
+      height: '60px',
+      width: '320px'
+    }
+
+    const styleRaisedButtonLabel = {
+      fontFamily: 'Montserrathairline',
+      fontSize: '1.8rem',
+      position: 'relative',
+      top: '17px'
+    }
+
     return <Row style={styleHero}>
       <Col lg={6} style={stylePipes}>
         <PipesHero />
       </Col>
-      <Col lg={6}>
-      <div style={styleCloud3}><img src="images/cloud_3.svg"/></div>
+      <Col lg={6} style={styleHeroHeader}>
+      <Row>
+        <Col xs={12}>
+          <div style={styleCloud3}><img src="images/cloud_3.svg"/></div>
+          <h2 >
+            JOB SEARCH MANAGMENT<br />
+            FOR A NEW ERA
+          </h2>
+        </Col>
+      </Row>
+      <Row style={styleHeroText}>
+        <Col xs={12}>
+          <p>Organize your interviews,</p>
+          <p>keep track of your contacts,</p>
+          <p>measure your progress,</p>
+          <p> and land your dream job.</p>
+        </Col>
+      </Row>
 
-      <h2 style={styleHeroHeader}>
-        JOB SEARCH MANAGMENT<br />
-        FOR A NEW ERA
-      </h2>
+      <Row>
+        <Col xs={12}>
+          <RaisedButton
+            backgroundColor={'#327F9E'}
+            label="sign up for free"
+            labelColor="white"
+            labelStyle={styleRaisedButtonLabel}
+            style={styleRaisedButton}
+          />
+      </Col>
+      </Row>
       </Col>
     </Row>
   }
