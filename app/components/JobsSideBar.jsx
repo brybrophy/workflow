127216ac1dcfc6f7579d024a2de0ddacb3d27f2f
Bryan Brophy <brybrophy@gmail.com>
2016-08-21 11:43:44 -0700

    Style Jobs Side Nav

--- app/components/JobsSideBar.jsx
+++ app/components/JobsSideBar.jsx
@@ -1,34 +1,93 @@
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
 import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
 import React from 'react';
+import SearchIcon from 'material-ui/svg-icons/action/search';
+import SelectField from 'material-ui/SelectField';
+import TextField from 'material-ui/TextField';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 const JobsSideBar = React.createClass({
   render() {
-
-    const styleSideMenu = {
-      display: 'inline-block',
+    const styleTextField = {
+      backgroundColor: 'white',
+      border: '1px solid lightgray',
+      borderRadius: '3px',
+      fontFamily: 'MontserratLight',
+      height: '45px',
+      margin: '5px',
+      padding: '5px 10px',
       width: '100%'
-    }
+    };
+
+    const styleTextFieldHint = {
+      color: '#EAE3DC',
+      fontFamily: 'MontserratHairline',
+      position: 'relative',
+      top: '5px'
+    };
 
     const styleFilter = {
-      borderBottom: '1px solid #EAE3DC',
       color: '#EAE3DC',
-      fontFamily: 'MontserratLight',
+      fontFamily: 'MontserratHairline',
       width: '86%'
     }
 
+    const styleFilterMenu = {
+      display: 'inline-block',
+      paddingBottom: '55px',
+      width: '100%'
+    }
+
+    const styleRaisedButton = {
+      height: '45px',
+      marginLeft: '5px',
+      width: '100%'
+    }
+
+    const styleRaisedButtonLabel = {
+      fontFamily: 'Montserrathairline',
+      fontSize: '1.8rem',
+      position: 'relative',
+      top: '10px'
+    }
+
+    const styleSearchMenu = {
+      backgroundColor: '#F9F8F7',
+      padding: '30px 30px 30px 20px'
+    }
+
     return <div>
-    <Paper style={styleSideMenu}>
-      <Menu>
-        <MenuItem primaryText="Company Name" style={styleFilter} />
-        <MenuItem primaryText="Job Title" style={styleFilter} />
-        <MenuItem primaryText="Newest" style={styleFilter} />
-        <MenuItem primaryText="Oldest" style={styleFilter} />
-        <MenuItem primaryText="Most Progress" style={styleFilter} />
-        <MenuItem primaryText="Least Progress" style={styleFilter} />
-      </Menu>
-    </Paper>
+      <Paper style={styleSearchMenu}>
+        <TextField
+          hintStyle={styleTextFieldHint}
+          hintText="Search"
+          style={styleTextField}
+          underlineShow={false}
+        />
+
+        <RaisedButton
+          label="add job"
+          labelColor="#EAE3DC"
+          labelStyle={styleRaisedButtonLabel}
+          style={styleRaisedButton}
+        />
+      </Paper>
+
+      <Paper style={styleFilterMenu}>
+        <Menu>
+          <MenuItem
+            primaryText="Company Name"
+            style={styleFilter}
+          />
+          <MenuItem primaryText="Job Title" style={styleFilter} />
+          <MenuItem primaryText="Newest" style={styleFilter} />
+          <MenuItem primaryText="Oldest" style={styleFilter} />
+          <MenuItem primaryText="Most Progress" style={styleFilter} />
+          <MenuItem primaryText="Least Progress" style={styleFilter} />
+        </Menu>
+      </Paper>
   </div>
   }
 });
