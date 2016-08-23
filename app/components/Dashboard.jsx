0abf30f425fd848d223af2c06963f001f117bbc1
Bryan Brophy <brybrophy@gmail.com>
2016-08-22 18:40:53 -0700

    Add missing curly in sass

--- app/components/Dashboard.jsx
+++ app/components/Dashboard.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Col, Grid, Row } from 'react-bootstrap';
 
 const Dashboard = React.createClass({
   render() {
     return <div>
-      Hello
+      <Grid>
+        <Row>
+          <Col xs={1}>Hello</Col>
+        </Row>
+      </Grid>
     </div>
   }
 });
