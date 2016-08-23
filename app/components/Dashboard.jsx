cf98e814377028a6bd6568e419b4950a67243e1a
Bryan Brophy <brybrophy@gmail.com>
2016-08-22 18:34:25 -0700

    Add dashboard client side route

---
+++ app/components/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = React.createClass({
+  render() {
+    return <div>
+      Hello
+    </div>
+  }
+});
+
+export default Dashboard;
--- app/components/Routes.jsx
+++ app/components/Routes.jsx
@@ -2,6 +2,7 @@ import { browserHistory, IndexRoute, Route, Router } from 'react-router';
 import App from 'components/App';
 import Authorize from 'components/Authorize';
 import ContactForm from 'components/ContactForm';
+import Dashboard from 'components/Dashboard';
 import JobInfo from 'components/JobInfo';
 import JobNotes from 'components/JobNotes';
 import JobProgress from 'components/JobProgress';
@@ -22,7 +23,7 @@ const Routes = React.createClass({
           <Route component={JobProgress} path="progress" />
           <Route component={JobNotes} path="notes" />
         </Route>
-        {/* <Route component={Dashboard} path="/dashboard" /> */}
+        <Route component={Dashboard} path="/dashboard" />
         <Route component={Jobs} path="jobs" />
         {/* <Route component={Contacts} path="contacts" /> */}
         {/* <Route component={NotFound} path="*" /> */}
