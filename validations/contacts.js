0852a33ff9420f430b9a34aa47d28d483fe43da3
Jake Bell <jakebrbell@gmail.com>
2016-08-23 20:02:48 -0700

    Add title and company columns to contacts validations

--- validations/contacts.js
+++ validations/contacts.js
@@ -7,30 +7,38 @@ module.exports.post = {
     firstName: Joi.string()
       .min(3)
       .max(31)
-      .label('firstName')
+      .label('First Name')
       .trim()
       .required(),
     lastName: Joi.string()
       .min(3)
       .max(31)
-      .label('lastName')
+      .label('Last Name')
       .trim()
       .required(),
     phoneNumber: Joi.string()
       .min(10)
       .max(13)
-      .label('phoneNumber')
+      .label('Phone Number')
       .trim()
       .required(),
     email: Joi.string()
       .email()
       .max(63)
-      .label('First name')
+      .label('Email')
       .trim()
       .required(),
+    title: Joi.string()
+      .min(3)
+      .label('Title')
+      .trim(),
+    company: Joi.string()
+      .min(2)
+      .label('Company')
+      .trim(),
     linkedInUrl: Joi.string()
       .max(63)
-      .label('linkedInUrl')
+      .label('LinkedIn URL')
       .trim()
       .required()
   }
@@ -41,26 +49,34 @@ module.exports.patch = {
     firstName: Joi.string()
       .min(3)
       .max(31)
-      .label('firstName')
+      .label('First Name')
       .trim(),
     lastName: Joi.string()
       .min(3)
       .max(31)
-      .label('lastName')
+      .label('Last Name')
       .trim(),
     phoneNumber: Joi.string()
       .min(10)
       .max(13)
-      .label('phoneNumber')
+      .label('Phone Number')
       .trim(),
     email: Joi.string()
       .email()
       .max(63)
-      .label('First name')
+      .label('Email')
+      .trim(),
+    title: Joi.string()
+      .min(3)
+      .label('Title')
+      .trim(),
+    company: Joi.string()
+      .min(2)
+      .label('Company')
       .trim(),
     linkedInUrl: Joi.string()
       .max(63)
-      .label('linkedInUrl')
+      .label('LinkedIn URL')
       .trim()
   }
 };
