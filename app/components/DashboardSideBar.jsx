bbda2a2bc91f7474d6cb4988c7826ff1a5f9324f
Jake Bell <jakebrbell@gmail.com>
2016-08-25 12:02:49 -0700

    Add link to Add Job button

--- app/components/DashboardSideBar.jsx
+++ app/components/DashboardSideBar.jsx
@@ -1,3 +1,4 @@
+import { Col } from 'react-bootstrap';
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
 import Paper from 'material-ui/Paper';
@@ -5,90 +6,91 @@ import RaisedButton from 'material-ui/RaisedButton';
 import React from 'react';
 import SelectField from 'material-ui/SelectField';
 import TextField from 'material-ui/TextField';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { withRouter } from 'react-router';
 
 const JobsSideBar = React.createClass({
+  handleTouchTap() {
+    this.props.router.push('/new/job');
+  },
+
   render() {
-    const styleTextField = {
-      backgroundColor: 'white',
-      border: '1px solid lightgray',
-      borderRadius: '3px',
-      fontFamily: 'MontserratLight',
-      height: '45px',
-      margin: '5px',
-      padding: '5px 10px',
-      width: '100%'
+    const styles = {
+      filter: {
+        color: '#A6A399',
+        fontFamily: 'MontserratHairline',
+        width: '86%'
+      },
+      filterMenu: {
+        display: 'inline-block',
+        paddingBottom: '55px',
+        width: '100%'
+      },
+      raisedButton: {
+        height: '45px',
+        marginLeft: '5px',
+        width: '100%'
+      },
+      raisedButtonLabel: {
+        fontFamily: 'Montserrathairline',
+        fontSize: '1.5rem',
+        position: 'relative',
+        top: '10px'
+      },
+      searchMenu: {
+        backgroundColor: '#F9F8F7',
+        padding: '30px 30px 30px 20px'
+      },
+      textField: {
+        backgroundColor: 'white',
+        border: '1px solid lightgray',
+        borderRadius: '3px',
+        fontFamily: 'MontserratLight',
+        height: '45px',
+        margin: '5px',
+        padding: '5px 10px',
+        width: '100%'
+      },
+      textFieldHint: {
+        color: '#A6A399',
+        fontFamily: 'MontserratHairline',
+        position: 'relative',
+        top: '5px'
+      }
     };
-
-    const styleTextFieldHint = {
-      color: '#A6A399',
-      fontFamily: 'MontserratHairline',
-      position: 'relative',
-      top: '5px'
-    };
-
-    const styleFilter = {
-      color: '#A6A399',
-      fontFamily: 'MontserratHairline',
-      width: '86%'
-    }
-
-    const styleFilterMenu = {
-      display: 'inline-block',
-      paddingBottom: '55px',
-      width: '100%'
-    }
-
-    const styleRaisedButton = {
-      height: '45px',
-      marginLeft: '5px',
-      width: '100%'
-    }
-
-    const styleRaisedButtonLabel = {
-      fontFamily: 'Montserrathairline',
-      fontSize: '1.5rem',
-      position: 'relative',
-      top: '10px'
-    }
-
-    const styleSearchMenu = {
-      backgroundColor: '#F9F8F7',
-      padding: '30px 30px 30px 20px'
-    }
 
     return <Col xs={12} md={3}>
-      <Paper style={styleSearchMenu}>
+      <Paper style={styles.searchMenu}>
         <TextField
-          hintStyle={styleTextFieldHint}
+          hintStyle={styles.textFieldHint}
           hintText="Search"
-          style={styleTextField}
+          style={styles.textField}
           underlineShow={false}
         />
 
         <RaisedButton
-          label="add job"
+          label="Add Job"
           labelColor="#A6A399"
-          labelStyle={styleRaisedButtonLabel}
-          style={styleRaisedButton}
+          labelStyle={styles.raisedButtonLabel}
+          onTouchTap={this.handleTouchTap}
+          style={styles.raisedButton}
         />
       </Paper>
 
-      <Paper style={styleFilterMenu}>
+      <Paper style={styles.filterMenu}>
         <Menu>
           <MenuItem
             primaryText="Company Name"
-            style={styleFilter}
+            style={styles.filter}
           />
-          <MenuItem primaryText="Job Title" style={styleFilter} />
-          <MenuItem primaryText="Newest" style={styleFilter} />
-          <MenuItem primaryText="Oldest" style={styleFilter} />
-          <MenuItem primaryText="Most Progress" style={styleFilter} />
-          <MenuItem primaryText="Least Progress" style={styleFilter} />
+          <MenuItem primaryText="Job Title" style={styles.filter} />
+          <MenuItem primaryText="Newest" style={styles.filter} />
+          <MenuItem primaryText="Oldest" style={styles.filter} />
+          <MenuItem primaryText="Most Progress" style={styles.filter} />
+          <MenuItem primaryText="Least Progress" style={styles.filter} />
         </Menu>
       </Paper>
   </Col>
   }
 });
 
-export default JobsSideBar;
+export default withRouter(JobsSideBar);
