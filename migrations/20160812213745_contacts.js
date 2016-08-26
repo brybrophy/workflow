4c2b75d566a2664fd9f8ea22f15bf27414c773a6
Jake Bell <jakebrbell@gmail.com>
2016-08-25 20:36:55 -0700

    Refactor contacts migration file

--- migrations/20160812213745_contacts.js
+++ migrations/20160812213745_contacts.js
@@ -5,13 +5,13 @@ exports.up = function(knex) {
     table.increments();
     table.string('first_name').notNullable().defaultTo('');
     table.string('last_name').notNullable().defaultTo('');
-    table.string('email').unique().notNullable().defaultTo('');
-    table.string('phone').unique().notNullable().defaultTo('');
+    table.string('email').notNullable().defaultTo('');
+    table.string('phone').notNullable().defaultTo('');
     table.string('title').notNullable().defaultTo('');
     table.string('company').notNullable().defaultTo('');
     table.string('linked_in_url').notNullable().defaultTo('');
     table.integer('user_id')
-    .notNullable()
+      .notNullable()
       .references('id')
       .inTable('users')
       .onDelete('CASCADE')
