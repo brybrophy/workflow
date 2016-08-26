d8f61d69134da1634d67fb8ad9f995579a8aed69
Bryan Brophy <brybrophy@gmail.com>
2016-08-26 14:51:02 -0700

    Add user id cookie on login and sign up

--- routes/users.js
+++ routes/users.js
@@ -5,17 +5,13 @@ const router = express.Router();
 
 const knex = require('../knex');
 
-const ev = require('express-validation');
-// const validations = require('../validations/users');
-// const { checkAuth } = require('../middleware');
-
 const boom = require('boom');
 const { camelizeKeys, decamelizeKeys } = require('humps');
 
 const appId = process.env.APP_ID;
 const appSecret = process.env.APP_SECRET;
 
-const Linkedin = require('node-linkedin')(appId, appSecret, 'https://workflow-react.herokuapp.com/api/users/oauth/linkedin/callback');
+const Linkedin = require('node-linkedin')(appId, appSecret, 'http://localhost:8000/api/users/oauth/linkedin/callback');
 
 const scope = ['r_basicprofile', 'r_emailaddress'];
 
@@ -26,7 +22,7 @@ router.get('/users/oauth/linkedin', function(req, res) {
 router.get('/users/oauth/linkedin/callback', function(req, res, next) {
   Linkedin.auth.getAccessToken(res, req.query.code, req.query.state, function(err, result) {
       if ( err ) {
-        console.error(err);
+        throw boom.create(400, 'Linkedin oauth failed');
         res.redirect('/auth');
       }
 
@@ -38,9 +34,7 @@ router.get('/users/oauth/linkedin/callback', function(req, res, next) {
         const email = $in.emailAddress;
         const accessToken = token;
 
-        console.log($in);
-
-        knex('users')
+        return knex('users')
           .select(knex.raw('1=1'))
           .where('linkedin_id', linkedinId)
           .first()
@@ -60,16 +54,15 @@ router.get('/users/oauth/linkedin/callback', function(req, res, next) {
                return knex('users').insert(row, '*');
           })
           .then((users) => {
-            res.send(users[0]);
+            res.cookie('userrId', users[0]['id']);
+            res.cookie('token', token);
+            return res.redirect('/dashboard');
           })
           .catch((err) => {
             next(err);
           });
       });
-
-      res.cookie('token', token);
-      return res.redirect('/dashboard');
-  });
+    });
 });
 
 module.exports = router;
