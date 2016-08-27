cc02f19b94eee7d5696bcb32b988760f2804059f
Jake Bell <jakebrbell@gmail.com>
2016-08-27 11:55:19 -0700

    Lint ContactView component

--- app/components/ContactView.jsx
+++ app/components/ContactView.jsx
@@ -18,31 +18,37 @@ const ContactView = React.createClass({
   render() {
     const { contacts } = this.props;
 
-    const styleColumn = {
-      padding: '0 3px'
+    const styles = {
+      column: {
+        padding: '0 3px'
+      },
+      flatButton: {
+        borderRadius: '3px',
+        fontFamily: 'MontserratHairline',
+        margin: '5px',
+        width: '100%'
+      },
+      grid: {
+        margin: '30px auto',
+        maxWidth: '700px'
+      },
+      paper: {
+        padding: '25px 75px',
+        marginLeft: '6px'
+      },
+      paragraph: {
+        fontSize: '16px',
+        fontWeight: 'bold',
+        lineHeight: '200%'
+      },
+      row: {
+        marginBottom: '20px'
+      }
     };
 
-    const styleFlatButton = {
-      borderRadius: '3px',
-      fontFamily: 'MontserratHairline',
-      margin: '5px',
-      width: '100%'
-    };
-
-    const stylePaper = {
-      padding: '25px 75px',
-      marginLeft: '6px'
-    };
-
-    const styleParagraph = {
-      fontSize: '16px',
-      fontWeight: 'bold',
-      lineHeight: '200%'
-    };
-
-    return <Grid style={{ margin: '30px auto', maxWidth: '700px' }}>
-      <Row style={{ marginBottom: '20px' }}>
-        <Col xs={12} sm={6} style={styleColumn}>
+    return <Grid style={styles.grid}>
+      <Row style={styles.row}>
+        <Col sm={6} style={styles.column} xs={12}>
           <FlatButton
             backgroundColor="#327F9E"
             disabled={this.props.editing.length !== 0}
@@ -50,48 +56,53 @@ const ContactView = React.createClass({
             label="Add a Contact"
             labelStyle={{ color: '#FFF' }}
             onTouchTap={this.handleTouchTapAdd}
-            style={styleFlatButton}
+            style={styles.flatButton}
           />
         </Col>
-        <Col xs={12} sm={6} style={styleColumn}>
+        <Col sm={6} style={styles.column} xs={12}>
           <FlatButton
             backgroundColor="#327F9E"
             hoverColor="#47B4E0"
             label="Save and Next"
             labelStyle={{ color: '#FFF' }}
             onTouchTap={this.handleTouchTapSave}
-            style={styleFlatButton}
+            style={styles.flatButton}
           />
         </Col>
       </Row>
       {contacts.length === 0
-        ?
+        ? // eslint-disable-line operator-linebreak
         <Row>
-          <Col xs={12} style={styleColumn}>
-            <Paper style={stylePaper} zDepth={2}>
-              <p style={styleParagraph}>Click "Add a Contact" to associate a contact with {this.props.job.title} at {this.props.job.companyName}. <br />
-              If you don't have any contacts for this job, click "Save and Next"</p>
+          <Col style={styles.column} xs={12}>
+            <Paper style={styles.paper} zDepth={2}>
+              <p style={styles.paragraph}>
+                Click "Add a Contact" to associate a contact with
+                {this.props.job.title} at {this.props.job.companyName}. <br />
+                If you don't have any contacts for this job,
+                click "Save and Next"
+              </p>
             </Paper>
           </Col>
         </Row>
-        :
+        : // eslint-disable-line operator-linebreak
         contacts.map((contact) => {
-        if (this.props.editing.indexOf(contact) !== -1) {
-          return <ContactForm
-            key={weakKey(contact)}
-            contact={contact}
-            stopEditingContact={this.props.stopEditingContact}
-            updateContact={this.props.updateContact}
-          />
-        }
+          if (this.props.editing.indexOf(contact) !== -1) {
+            return <ContactForm
+              contact={contact}
+              key={weakKey(contact)}
+              stopEditingContact={this.props.stopEditingContact}
+              updateContact={this.props.updateContact}
+            />;
+          }
 
-        return <Contact
-          key={weakKey(contact)}
-          openDeleteDialog={this.props.openDeleteDialog}
-          contact={contact}
-          startEditingContact={this.props.startEditingContact}
-        />
-      })}
+          return <Contact
+            contact={contact}
+            key={weakKey(contact)}
+            openDeleteDialog={this.props.openDeleteDialog}
+            startEditingContact={this.props.startEditingContact}
+          />;
+        })
+      }
     </Grid>;
   }
 });
