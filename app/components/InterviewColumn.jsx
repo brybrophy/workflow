25500f4a4c071ee884f1ac514986d8ef69aeccab
Jake Bell <jakebrbell@gmail.com>
2016-08-27 12:41:31 -0700

    Lint InterviewColumn component

--- app/components/InterviewColumn.jsx
+++ app/components/InterviewColumn.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 import { TableRowColumn } from 'material-ui/Table';
 import Timestamp from 'react-timestamp';
 
-const InterviewColumn  = React.createClass({
+const InterviewColumn = React.createClass({
   render() {
     const job = this.props.job;
     const name = this.props.name;
     let interviewDate = '';
     let interviewTime = '';
-    if (job[name]['date']) {
-      interviewDate = <Timestamp time={job[name]['date']} format="date" />;
-      interviewTime = <Timestamp time={job[name]['date']} format="time" />;
+
+    if (job[name].date) {
+      interviewDate = <Timestamp format="date" time={job[name].date} />;
+      interviewTime = <Timestamp format="time" time={job[name].date} />;
     }
 
     return <TableRowColumn>
       {interviewDate}<br />{interviewTime}
-    </TableRowColumn>
+    </TableRowColumn>;
   }
 });
 
