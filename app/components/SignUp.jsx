02d659db26d1303d9047627a1edbf9527f6fef1f
Jake Bell <jakebrbell@gmail.com>
2016-08-27 16:21:20 -0700

    Lint SignUp component

--- app/components/SignUp.jsx
+++ app/components/SignUp.jsx
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import FlatButton from 'material-ui/FlatButton';
 import LinkedInLogo from 'components/LinkedInLogo';
 import React from 'react';
@@ -13,101 +12,96 @@ const SignUp = React.createClass({
   },
 
   render() {
-    const styleBox = {
-      backgroundColor: 'white',
-      border: '1px solid #D1CFC8',
-      borderRadius: '3px',
-      height: '445px',
-      position: 'relative',
-      right: '50px',
-      top: '165px',
-      width: '35vw'
+    const styles = {
+      box: {
+        backgroundColor: 'white',
+        border: '1px solid #D1CFC8',
+        borderRadius: '3px',
+        height: '445px',
+        position: 'relative',
+        right: '50px',
+        top: '165px',
+        width: '35vw'
+      },
+      buttonHeader: {
+        color: '#A6A399',
+        fontFamily: 'MontserratLight',
+        textAlign: 'center',
+        marginTop: '20%'
+      },
+      flatButton1: {
+        backgroundColor: '#E7E4DB',
+        borderRadius: '3px 0 0 0',
+        borderBottom: '1px solid #D1CFC8',
+        borderRight: '1px solid #D1CFC8',
+        fontFamily: 'MontserratLight',
+        height: '60px',
+        width: '50%'
+      },
+      flatButton2: {
+        backgroundColor: 'white',
+        borderRadius: '0 3px 0 0',
+        fontFamily: 'MontserratLight',
+        height: '60px',
+        width: '50%'
+      },
+      flatButtonAlt: {
+        backgroundColor: '#E7E4DB',
+        borderRadius: '3px',
+        fontFamily: 'MontserratLight',
+        height: '70px',
+        margin: '10px 23% 15% 23%',
+        paddingTop: '15px',
+        postiton: 'relative',
+        width: '55%'
+      },
+      flatButtonLabel: {
+        color: '#A6A399',
+        fontFamily: 'MontserratLight',
+        fontSize: '1.4rem',
+        letterSpacing: '0.12rem'
+      },
+      flatButtonLabelAlt: {
+        color: '#A6A399',
+        fontFamily: 'MontserratLight',
+        fontSize: '1.4rem',
+        letterSpacing: '0.12rem',
+        position: 'relative',
+        bottom: '13px'
+      },
+      smallText: {
+        color: '#A6A399',
+        fontFamily: 'MontserratLight',
+        fontSize: '1.3rem',
+        textAlign: 'center'
+      }
     };
 
-    const styleButtonHeader ={
-      color: '#A6A399',
-      fontFamily: 'MontserratLight',
-      textAlign: 'center',
-      marginTop: '20%'
-    };
-
-    const styleFlatButton1 = {
-      backgroundColor: '#E7E4DB',
-      borderRadius: '3px 0 0 0',
-      borderBottom: '1px solid #D1CFC8',
-      borderRight: '1px solid #D1CFC8',
-      fontFamily: 'MontserratLight',
-      height: '60px',
-      width: '50%'
-    };
-
-    const styleFlatButton2 = {
-      backgroundColor: 'white',
-      borderRadius: '0 3px 0 0',
-      fontFamily: 'MontserratLight',
-      height: '60px',
-      width: '50%'
-    };
-
-    const styleFlatButtonAlt = {
-      backgroundColor: '#E7E4DB',
-      borderRadius: '3px',
-      fontFamily: 'MontserratLight',
-      height: '70px',
-      margin: '10px 23% 15% 23%',
-      paddingTop: '15px',
-      postiton: 'relative',
-      width: '55%'
-    };
-
-    const styleFlatButtonLabel = {
-      color: '#A6A399',
-      fontFamily: 'MontserratLight',
-      fontSize: '1.4rem',
-      letterSpacing: '0.12rem',
-    };
-
-    const styleFlatButtonLabelAlt = {
-      color: '#A6A399',
-      fontFamily: 'MontserratLight',
-      fontSize: '1.4rem',
-      letterSpacing: '0.12rem',
-      position: 'relative',
-      bottom: '13px'
-    };
-
-    const styleSmallText = {
-      color: '#A6A399',
-      fontFamily: 'MontserratLight',
-      fontSize: '1.3rem',
-      textAlign: 'center'
-    };
-
-    return <div style={styleBox}>
+    return <div style={styles.box}>
       <FlatButton
         label="Login"
-        labelStyle={styleFlatButtonLabel}
+        labelStyle={styles.flatButtonLabel}
         onTouchTap={this.handleTouchTap}
-        style={styleFlatButton1}
+        style={styles.flatButton1}
       />
 
       <FlatButton
         label="Sign Up"
-        labelStyle={styleFlatButtonLabel}
-        style={styleFlatButton2}
+        labelStyle={styles.flatButtonLabel}
+        style={styles.flatButton2}
       />
 
-      <h4 style={styleButtonHeader}>SIGN UP WITH</h4>
+      <h4 style={styles.buttonHeader}>SIGN UP WITH</h4>
 
       <FlatButton
-        label="LinkedIn"
-        labelStyle={styleFlatButtonLabelAlt}
-        onTouchTap={this.handleSignUp}
         icon={<LinkedInLogo />}
-        style={styleFlatButtonAlt}
+        label="LinkedIn"
+        labelStyle={styles.flatButtonLabelAlt}
+        onTouchTap={this.handleSignUp}
+        style={styles.flatButtonAlt}
       />
 
-      <div style={styleSmallText}>
+      <div style={styles.smallText}>
         <small>
           You must have a LinkedIn account to use this application.<br />
           If you do not, click the link above or go
@@ -117,7 +111,7 @@ const SignUp = React.createClass({
           </a>
         </small>
       </div>
-    </div>
+    </div>;
   }
 });
 
