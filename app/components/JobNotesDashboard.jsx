ac7d61391b4f09afc0286d2bbe9e47b3d4c3e9a7
Jake Bell <jakebrbell@gmail.com>
2016-08-27 13:14:46 -0700

    Lint JobNotesDashboard component

--- app/components/JobNotesDashboard.jsx
+++ app/components/JobNotesDashboard.jsx
@@ -5,26 +5,26 @@ import React from 'react';
 
 const JobNotesDashboard = React.createClass({
   handleTouchTap() {
-    this.props.onHandleEditing(this.props.job, this.props.id)
+    this.props.onHandleEditing(this.props.job, this.props.id);
   },
-  
+
   render() {
     const job = this.props.job;
     const styles = this.props.styles;
 
     return <div>
-      <h4 style={{display: 'inline-block'}}>Notes</h4>
+      <h4 style={{ display: 'inline-block' }}>Notes</h4>
       <FlatButton
         icon={<EditMode />}
         label="Edit"
         onTouchTap={this.handleTouchTap}
         primary={true}
-        style={{float: 'right'}}
+        style={{ float: 'right' }}
       />
       <Paper style={styles.section}>
         <p style={styles.paragraph}>{job.notes}</p>
       </Paper>
-    </div>
+    </div>;
   }
 });
 
