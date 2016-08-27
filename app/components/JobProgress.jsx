809e4e6004737decdacabd820a9293176cd327cb
Jake Bell <jakebrbell@gmail.com>
2016-08-27 13:26:52 -0700

    Lint JobProgress component

--- app/components/JobProgress.jsx
+++ app/components/JobProgress.jsx
@@ -1,19 +1,13 @@
 import {
   Table,
   TableBody,
-  TableFooter,
   TableHeader,
   TableHeaderColumn,
-  TableRow,
-  TableRowColumn
+  TableRow
 } from 'material-ui/Table';
-import DatePicker from 'material-ui/DatePicker';
-import FlatButton from 'material-ui/FlatButton';
 import InterviewColumnEdit from 'components/InterviewColumnEdit';
 import Paper from 'material-ui/Paper';
 import React from 'react';
-import Timestamp from 'react-timestamp';
-import TimePicker from 'material-ui/TimePicker';
 
 const interviewSteps = [
   'interviewApplied',
@@ -35,25 +29,30 @@ const interviewHeaders = [
   'OFFER RECIEVED'
 ];
 
+const monthNames = [
+  'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+  'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+];
+
 const JobProgress = React.createClass({
   getInitialState() {
     return {
       job: this.props.job
-    }
+    };
   },
 
   handleTouchTapSave() {
     const nextJob = Object.assign({}, this.state.job);
 
     for (const step of interviewSteps) {
-      if (nextJob[step]['date'] && nextJob[step]['time']) {
-        const date = nextJob[step]['date'];
-        const time = nextJob[step]['time'];
+      if (nextJob[step].date && nextJob[step].time) {
+        const date = nextJob[step].date;
+        const time = nextJob[step].time;
 
         const nextInterview = new Date(`${date} ${time}`);
 
-        nextJob[step]['date'] = nextInterview;
-        nextJob[step]['time'] = '';
+        nextJob[step].date = nextInterview;
+        nextJob[step].time = '';
       }
     }
 
@@ -65,12 +64,18 @@ const JobProgress = React.createClass({
     const day = date.getDate();
     const month = date.getMonth();
     const year = date.getFullYear();
-    const formatted =  `${day} ${monthNames[month]} ${year}`;
+    const formatted = `${day} ${monthNames[month]} ${year}`;
+
     return formatted;
   },
 
   render() {
     const styles = {
+      header: {
+        fontSize: '30px',
+        margin: '30px',
+        textAlign: 'center'
+      },
       section: {
         backgroundColor: 'white',
         borderRadius: '5px',
@@ -81,34 +86,35 @@ const JobProgress = React.createClass({
         marginBottom: '20px'
       }
     };
-    const job = this.props.job;
 
     return <div>
-    <h4 style={{fontSize: '30px', margin: '30px', textAlign: 'center' }}>Add Interview Progress Points</h4>
-    <Paper style={styles.section}>
-    <Table style={styles.table} selectable={false}>
-      <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
-        <TableRow>
-          {interviewHeaders.map((header, index) => {
-            return <TableHeaderColumn key={index}>{header}</TableHeaderColumn>
-          })}
-        </TableRow>
-      </TableHeader>
-      <TableBody displayRowCheckbox={false}>
-        <TableRow>
-        {interviewSteps.map((step, index) => {
-          return <InterviewColumnEdit
-            job={this.props.job}
-            key={index}
-            name={step}
-            updateInterviewStep={this.props.updateInterviewStep}
-          />;
-        })}
-        </TableRow>
-      </TableBody>
-    </Table>
-    </Paper>
-    </div>
+      <h4 style={styles.header}>Add Interview Progress Points</h4>
+      <Paper style={styles.section}>
+        <Table selectable={false} style={styles.table}>
+          <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
+            <TableRow>
+              {interviewHeaders.map((header, index) => {
+                return <TableHeaderColumn key={index}>
+                  {header}
+                </TableHeaderColumn>;
+              })}
+            </TableRow>
+          </TableHeader>
+          <TableBody displayRowCheckbox={false}>
+            <TableRow>
+            {interviewSteps.map((step, index) => {
+              return <InterviewColumnEdit
+                job={this.props.job}
+                key={index}
+                name={step}
+                updateInterviewStep={this.props.updateInterviewStep}
+              />;
+            })}
+            </TableRow>
+          </TableBody>
+        </Table>
+      </Paper>
+    </div>;
   }
 });
 
