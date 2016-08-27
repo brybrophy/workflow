285c9062a75028eb533f29a42d68c56b0cf7fd38
Jake Bell <jakebrbell@gmail.com>
2016-08-27 13:13:25 -0700

    Lint JobNotes component

--- app/components/JobNotes.jsx
+++ app/components/JobNotes.jsx
@@ -1,4 +1,4 @@
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Col, Grid, Row } from 'react-bootstrap';
 import FlatButton from 'material-ui/FlatButton';
 import Paper from 'material-ui/Paper';
 import React from 'react';
@@ -14,48 +14,50 @@ const JobNotes = React.createClass({
   },
 
   render() {
-    const styleColumn = {
-      padding: '10px 3px'
-    };
-
-    const stylePaper = {
-      padding: '25px 75px'
-    };
-
-    const styleFlatButton = {
-      borderRadius: '3px',
-      fontFamily: 'MontserratHairline',
-      margin: '5px',
-      width: '100%'
-    };
-
-    const styleTextField = {
-      fontFamily: 'MontserratLight'
+    const styles = {
+      column: {
+        padding: '10px 3px'
+      },
+      flatButton: {
+        borderRadius: '3px',
+        fontFamily: 'MontserratHairline',
+        margin: '5px',
+        width: '100%'
+      },
+      flatButtonLabel: {
+        color: 'white'
+      },
+      paper: {
+        padding: '25px 75px'
+      },
+      textField: {
+        fontFamily: 'MontserratLight'
+      }
     };
 
     return <Grid style={{ margin: '20px auto', maxWidth: '700px' }}>
       <Row style={{ marginBottom: '20px' }}>
-        <Col xs={12} sm={6} smOffset={6} style={styleColumn}>
+        <Col sm={6} smOffset={6} style={styles.column} xs={12}>
           <FlatButton
             backgroundColor="#327F9E"
             hoverColor="#47B4E0"
             label="Save and Finish"
-            labelStyle={{ color: 'white' }}
+            labelStyle={styles.flatButtonLabel}
             onTouchTap={this.handleSaveTouchTap}
-            style={styleFlatButton}
+            style={styles.flatButton}
           />
         </Col>
       </Row>
       <Row>
         <Col>
-          <Paper style={stylePaper}>
+          <Paper style={styles.paper}>
             <TextField
               fullWidth={true}
               hintText="Type job notes here"
               multiLine={true}
               name="notes"
               onChange={this.handleChange}
-              style={styleTextField}
+              style={styles.textField}
               underlineShow={false}
               value={this.props.job.notes}
             />
