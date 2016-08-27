56480cefdc2b05a0bad38c3317a3cb5efe9b5ea3
Jake Bell <jakebrbell@gmail.com>
2016-08-27 11:35:11 -0700

    Lint Contact component

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,4 +1,4 @@
-import { Row, Col } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import Paper from 'material-ui/Paper';
 import React from 'react';
 
@@ -14,48 +14,77 @@ const Contact = React.createClass({
   render() {
     const { contact } = this.props;
 
-    const styleLine = {
-      fontFamily: 'MontserratHairline',
-      fontSize: '4rem',
-      margin: '60px 5px 0 15px',
-      position: 'relative',
-      right: '10px'
+    const styles = {
+      anchor: {
+        cursor: 'pointer',
+        display: 'inline-block',
+        margin: '10px'
+      },
+      editDeleteButtons: {
+        float: 'right'
+      },
+      header: {
+        display: 'inline-block',
+        fontFamily: 'MontserratLight'
+      },
+      line: {
+        fontFamily: 'MontserratHairline',
+        fontSize: '4rem',
+        margin: '60px 5px 0 15px',
+        position: 'relative',
+        right: '10px'
+      },
+      linkedInUrl: {
+        float: 'right',
+        marginTop: '60px'
+      },
+      paper: {
+        color: '#A9A28E',
+        height: '175px',
+        padding: '0 20px'
+      },
+      phone: {
+        marginTop: '30px'
+      },
+      row: {
+        margin: '20px auto',
+        maxWidth: '1000px'
+      }
     };
 
-    const stylePaper = {
-      color: '#A9A28E',
-      height: '175px',
-      padding: '0 20px'
-    };
-
-    return <Row style={{ margin: '20px auto', maxWidth: '1000px' }} >
+    return <Row style={styles.row}>
       <Col xs={12}>
-        <Paper style={stylePaper} zDepth={2}>
+        <Paper style={styles.paper} zDepth={2}>
           <Row>
             <Col xs={12}>
-              <h2
-                style={{display: 'inline-block', fontFamily: 'MontserratLight'}}
-              >
+              <h2 style={styles.header}>
                 {contact.firstName} {contact.lastName}
-                <span style={styleLine}> | </span>
+                <span style={styles.line}> | </span>
               </h2>
-              <h4 style={{display: 'inline-block', fontFamily: 'MontserratLight'}}>{contact.title} at {contact.company}</h4>
-              <p style={{ float: 'right' }}><a style={{ cursor: 'pointer', display: 'inline-block', margin: '10px' }} onTouchTap={this.handleTouchTapEdit}>
-                edit
-              </a></p>
-
-              <p style={{ float: 'right' }}><a style={{ cursor: 'pointer', display: 'inline-block', margin: '10px' }} onTouchTap={this.handleTouchTapDelete}>
-                delete
-              </a></p>
+              <h4 style={styles.header}>
+                {contact.title} at {contact.company}
+              </h4>
+              <p style={styles.editDeleteButtons}>
+                <a onTouchTap={this.handleTouchTapEdit} style={styles.anchor}>
+                  edit
+                </a>
+              </p>
+              <p style={styles.editDeleteButtons}>
+                <a onTouchTap={this.handleTouchTapDelete} style={styles.anchor}>
+                  delete
+                </a>
+              </p>
             </Col>
           </Row>
-          <Row >
+          <Row>
             <Col xs={4}>
-              <p style={{marginTop: '30px'}}>{contact.phone}</p>
+              <p style={styles.phone}>{contact.phone}</p>
               <p>{contact.email}</p>
             </Col>
             <Col xs={8}>
-              <p style={{float: 'right', marginTop: '60px'}}>{contact.linkedInUrl}</p>
+              <p style={styles.linkedInUrl}>
+                {contact.linkedInUrl}
+              </p>
             </Col>
           </Row>
         </Paper>
