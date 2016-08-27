9c40f59271f7693a959eaaa88607c59173c797af
Jake Bell <jakebrbell@gmail.com>
2016-08-27 14:41:01 -0700

    Lint MainNav component

--- app/components/MainNav.jsx
+++ app/components/MainNav.jsx
@@ -17,96 +17,94 @@ const MainNav = React.createClass({
       return this.props.router.push('/dashboard');
     }
 
-    const path = '/' + event.currentTarget.textContent.toLowerCase();
+    const path = `/${event.currentTarget.textContent.toLowerCase()}`;
+
     this.props.router.push(path);
   },
 
   render() {
-    const styleAppBar = {
-      height: '125px',
-      paddingRight: '70px'
+    const styles = {
+      appBar: {
+        height: '125px',
+        paddingRight: '70px'
+      },
+      flatButton: {
+        height: '125px',
+        lineHeight: '125px',
+        margin: '0 10px',
+        minWidth: '60px'
+      },
+      flatButtonLabel: {
+        fontFamily: 'MontserratHairline',
+        fontSize: '1.2rem',
+        letterSpacing: '0.12rem',
+        padding: '0'
+      },
+      iconLeft: {
+        height: '125px',
+        lineHeight: '125px',
+        marginLeft: '35px',
+        marginTop: '20px'
+      },
+      navbarLine: {
+        color: 'white',
+        fontFamily: 'MontserratHairline',
+        fontSize: '2rem',
+        marginLeft: '18px',
+        marginRight: '15px',
+        paddingTop: '30px'
+      },
+      subTitle: {
+        fontSize: '1.7rem',
+        marginLeft: '10px',
+        position: 'relative',
+        top: '-2px'
+      },
+      title: {
+        cursor: 'pointer',
+        fontFamily: 'MontserratHairline',
+        fontSize: '1.2rem',
+        height: '125px',
+        letterSpacing: '0.12rem',
+        lineHeight: '71px',
+        marginTop: '20px'
+      }
     };
 
-    const styleFlatButton = {
-      height: '125px',
-      lineHeight: '125px',
-      margin: '0 10px',
-      minWidth: '60px'
-    };
-
-    const styleFlatButtonLabel = {
-      fontFamily: 'MontserratHairline',
-      fontSize: '1.2rem',
-      letterSpacing: '0.12rem',
-      padding: '0'
-    };
-
-    const styleIconLeft = {
-      height: '125px',
-      lineHeight: '125px',
-      marginLeft: '35px',
-      marginTop: '20px'
-    };
-
-    const styleNavbarLine = {
-      color: 'white',
-      fontFamily: 'MontserratHairline',
-      fontSize: '2rem',
-      marginLeft: '18px',
-      marginRight: '15px',
-      paddingTop: '30px'
-    };
-
-    const styleSubTitle = {
-      fontSize: '1.7rem',
-      marginLeft: '10px',
-      position: 'relative',
-      top: '-2px'
-    };
-
-    const styleTitle = {
-      cursor: 'pointer',
-      fontFamily: 'MontserratHairline',
-      fontSize: '1.2rem',
-      height: '125px',
-      letterSpacing: '0.12rem',
-      lineHeight: '71px',
-      marginTop: '20px'
-    };
-
-
-    return <div><AppBar
-      iconElementLeft={<NavLogo />}
-      iconStyleLeft={styleIconLeft}
-      onTitleTouchTap={this.handleTouchTap}
-      style={styleAppBar}
-      title={<h1>workflow |
-        <span style={styleSubTitle}>Job Search Management</span>
-      </h1>}
-        titleStyle={styleTitle}
+    return <div>
+      <AppBar
+        iconElementLeft={<NavLogo />}
+        iconStyleLeft={styles.iconLeft}
+        onTitleTouchTap={this.handleTouchTap}
+        style={styles.appBar}
+        title={
+          <h1>workflow |
+            <span style={styles.subTitle}>Job Search Management</span>
+          </h1>
+        }
+        titleStyle={styles.title}
       >
-      <FlatButton
-        label="Dashboard"
-        labelStyle={styleFlatButtonLabel}
-        onTouchTap={this.handleTouchTap}
-        style={styleFlatButton}
-      />
-      <FlatButton
-        label="Contacts"
-        labelStyle={styleFlatButtonLabel}
-        onTouchTap={this.handleTouchTap}
-        style={styleFlatButton}
-      />
-
-      <h1 style={styleNavbarLine}> | </h1>
-
-      <FlatButton
-        label="Logout"
-        labelStyle={styleFlatButtonLabel}
-        onTouchTap={this.handleTouchTap}
-        style={styleFlatButton}
-      />
-    </AppBar>{this.props.children}</div>;
+        <FlatButton
+          label="Dashboard"
+          labelStyle={styles.flatButtonLabel}
+          onTouchTap={this.handleTouchTap}
+          style={styles.flatButton}
+        />
+        <FlatButton
+          label="Contacts"
+          labelStyle={styles.flatButtonLabel}
+          onTouchTap={this.handleTouchTap}
+          style={styles.flatButton}
+        />
+        <h1 style={styles.navbarLine}> | </h1>
+        <FlatButton
+          label="Logout"
+          labelStyle={styles.flatButtonLabel}
+          onTouchTap={this.handleTouchTap}
+          style={styles.flatButton}
+        />
+      </AppBar>{this.props.children}
+    </div>;
   }
 });
 
